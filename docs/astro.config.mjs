2727bd34746910df69ce412e3976cf65d49dc7f9
Andrew Lee <andrew.lee@applydigital.com>
2025-11-25 01:08:30 -0800

    add tailwind and custom theme

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,25 +2,36 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
+  integrations: [
+      starlight({
+          title: 'Apply Ai Engineering Workshop',
+          social: [{ icon: 'external', label: 'Website', href: 'https://https://www.applydigital.com/' }],
+          sidebar: [
+              {
+                  label: 'Guides',
+                  items: [
+                      { label: 'Claude Setup - VS Code', slug: 'guides/claude-setup-vs-code' },
+                      { label: 'Claude Setup - Terminal', slug: 'guides/claude-setup-terminal' },
+                      { label: 'Adding a MCP', slug: 'guides/adding-mcp'},
+                      { label: 'Adding a Claude Hook', slug: 'guides/add-a-claude-hook'}
+
+                  ],
+              },
+              {
+                  label: 'Reference',
+                  autogenerate: { directory: 'reference' },
+              },
+          ],
+		  customCss: [
+            './src/styles/global.css',
+        ],
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
